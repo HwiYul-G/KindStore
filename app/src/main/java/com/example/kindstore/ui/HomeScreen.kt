eca00950b96e5cafd32b6e9fc67a805c4b2bb76c
kim <hwiyul_g@daum.net>
2024-02-08 21:38:41 +0900

    feat(HomeScreen) : Scrollable Column으로 생성

--- app/src/main/java/com/example/kindstore/ui/HomeScreen.kt
+++ app/src/main/java/com/example/kindstore/ui/HomeScreen.kt
@@ -4,7 +4,12 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,14 +23,20 @@ import com.example.kindstore.R
 
 @Composable
 fun HomeScreen() {
-    Column() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(scrollState)
+    ) {
         ExplainText(
             R.string.what_is_good_shop_title,
             R.string.what_is_good_shop_content,
             R.drawable.goodshop_image,
             "가게를 배경으로 여러 사람들이 서있는 이미지"
         )
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(24.dp))
         ExplainText(
             R.string.goodshop_sticker_title,
             R.string.goodshop_sticker_content,
@@ -36,17 +47,27 @@ fun HomeScreen() {
 }
 
 @Composable
-private fun ExplainText(@StringRes title: Int, @StringRes content: Int, image: Int, imageDescription : String) {
+private fun ExplainText(
+    @StringRes title: Int,
+    @StringRes content: Int,
+    image: Int,
+    imageDescription: String
+) {
     Column {
-        Text(text = stringResource(id = title), fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = stringResource(id = content), fontSize = 14.sp, fontWeight = FontWeight.Normal)
-        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = stringResource(id = title), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = content),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            lineHeight = 28.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Image(painter = painterResource(id = image), contentDescription = imageDescription)
     }
 }
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewHomeScreen() {
     HomeScreen()
